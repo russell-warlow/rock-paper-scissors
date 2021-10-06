f754031fd198fb6cb2af5ce022d896c696c92a6c
Russell Warlow <52016758+russell-warlow@users.noreply.github.com>
2021-10-06 15:11:06 -0700

    updated ui

--- gaming-1.0.js
+++ gaming-1.0.js
@@ -11,18 +11,11 @@ function computerPlay() {
 }
 
 function playRound(playerSelection, computerSelection) {
-    let playerLowered = playerSelection.toLowerCase();
-    if(!(["rock", "paper", "scissors"].includes(playerLowered))) {
-        console.log("error: invalid player input: " + playerSelectionLowered);
-        return;
-    }
     let result = null;
-    let playerFormatted = playerLowered.charAt(0).toUpperCase() + playerLowered.substring(1, playerLowered.length);
-    //console.log("pF: " + playerFormatted + ", cS: " + computerSelection); 
-    if(playerFormatted == computerSelection) {
+    if(playerSelection == computerSelection) {
         result = TIE;
     }
-    else if (playerFormatted == ROCK) {
+    else if (playerSelection == ROCK) {
         if(computerSelection == PAPER) {
             result = LOSE;
         }
@@ -30,7 +23,7 @@ function playRound(playerSelection, computerSelection) {
             result = WIN;
         }
     }
-    else if(playerFormatted == PAPER) {
+    else if(playerSelection == PAPER) {
         if(computerSelection == ROCK) {
             result = WIN;
         }
@@ -46,34 +39,60 @@ function playRound(playerSelection, computerSelection) {
             result = WIN;
         }
     }
-    console.log("you: " + playerFormatted + ", computer: " + computerSelection + ", result: " + result);
     return result;
 }
  
-function game() {
-    result = window.prompt("enter rock, paper, or scissors: ");
-    console.log("playing five rounds ...");
-    let yourWins = 0;
-    let compWins = 0;
-    for(let i=0; i<5; i++) {
-        let matchResult = playRound(result, computerPlay());
-        if(matchResult < 0) {
-            compWins++;
-        }
-        else if(matchResult > 0) {
-            yourWins++;
+function play(humanChoice) {
+    let computerChoice = computerPlay();
+    let matchResult = playRound(humanChoice, computerChoice);
+    outputLog("new round. you (" + humanChoice + "), computer (" + computerChoice + ")");
+    if(matchResult < 0) {
+        outputLog("computer wins round");
+        let score = updateScore('computerScore');
+        if (score == 5) {
+            outputLog("COMPUTER WINS GAME! game ends ... ");
+            document.getElementById('computerScore').style.color = "red";
+            endGame();
         }
     }
-    console.log("your wins: " + yourWins + ", comp wins: " + compWins);
-    if(yourWins > compWins) {
-        console.log("you win!");
-    }
-    else if(compWins > yourWins) {
-        console.log("you lose!");
+    else if(matchResult > 0) {
+        outputLog("you win round!");
+        let score = updateScore('humanScore');
+        if (score == 5) {
+            outputLog("YOU WIN GAME! game ends ... ");
+            document.getElementById('humanScore').style.color = "red";
+            endGame();
+        }
     }
     else {
-        console.log("you tied!");
+        outputLog("you and computer tie!");
     }
 }
 
-game();
+function updateScore(id) {
+    let oldScore = document.getElementById(id).textContent;
+    let newScore = parseInt(oldScore) + 1;
+    document.getElementById(id).textContent = newScore;
+    return newScore
+}
+
+function outputLog(text) {
+    let oldText = document.getElementById('output').innerText;
+    let newText = oldText + '\n' + text;
+    document.getElementById('output').innerText = newText;
+}
+
+function clickEvent(e) {
+    let clickSelection = e.target.innerText;
+    play(clickSelection);
+}
+
+function endGame() {
+    let buttons = document.getElementsByClassName('button-40');
+    Array.from(buttons).forEach(element => { element.removeEventListener('click', clickEvent) });
+    outputLog('removed event listeners ... ');
+}
+
+let buttons = document.getElementsByClassName('button-40');
+Array.from(buttons).forEach(element => { element.addEventListener('click', clickEvent) });
+outputLog('added event listeners ...');
